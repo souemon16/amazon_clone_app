9f71e63bfa6e9c8a661f237c4a63ce5a5cd652bd
Sourav Sarkar Emon <souemon16@gmail.com>
2022-06-12 11:32:25 +0600

    server added to our project

---
+++ server/index.js
@@ -0,0 +1,25 @@
+// IMPORT FROM PACKAGES 
+const express = require("express");
+const mongoose = require("mongoose");
+// IMPORT FROM OTHER FILES 
+const authRouter = require("./routes/auth");
+// INIT 
+const PORT = 3000;
+const app = express();
+const DB = "mongodb+srv://souemon:amazonclone@cluster0.ifklh.mongodb.net/?retryWrites=true&w=majority";
+
+// MIDDLEWARE 
+app.use(authRouter);
+
+// DATABASE CONNECTIONS 
+mongoose.connect(DB).then(() => {
+    console.log("connection succesfully");
+}).catch((e) => {
+    console.log(e);
+})
+
+
+
+app.listen(PORT, 0,0,0,0, () => {
+    console.log(`Connected to PORT ${PORT}`)
+})
